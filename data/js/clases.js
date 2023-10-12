e24257362c15ce2a68ee33c4e66eac9466bc3506
ximenaortiz01 <ortizsanchezmariaximena@gmail.com>
2023-10-12 14:59:05 -0500

    inicio

---
+++ data/js/clases.js
@@ -0,0 +1,11 @@
+export class Gift {
+    constructor(id, gift, tipo, tiempo, precio, imagen) {
+      this.id = id;
+      this.gift = gift;
+      this.tipo = tipo;
+      this.tiempo = tiempo;
+      this.precio = precio;
+      this.imagen = imagen;
+    }
+  }
+  
